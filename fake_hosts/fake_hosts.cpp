a502b84066f8af530b201201541463986e2a32e3
umaumax <umauma7083@gmail.com>
2018-12-30 03:03:21 +0900

    Add inet_aton to fake_hosts

--- fake_hosts/fake_hosts.cpp
+++ fake_hosts/fake_hosts.cpp
@@ -8,15 +8,7 @@
 #include <sstream>
 #include <vector>
 
-extern "C" {
-struct hostent *gethostbyname2(const char *name, int af) {
-  using libc_gethostbyname2_pointer_type                      = decltype(&gethostbyname2);
-  static libc_gethostbyname2_pointer_type libc_gethostbyname2 = nullptr;
-  if (libc_gethostbyname2 == nullptr) {
-    libc_gethostbyname2 = (libc_gethostbyname2_pointer_type)dlsym(RTLD_NEXT, "gethostbyname2");
-    assert(libc_gethostbyname2 && "failed dlsym gethostbyname2");
-  }
-
+void parse_hosts(std::function<bool(std::string, std::string)> lambda) {
   // NOTE: priority
   // 1. $FAKE_HOSTS_FILEPATH
   // 2. ./.hosts
@@ -50,13 +42,46 @@ struct hostent *gethostbyname2(const char *name, int af) {
         }
         ss >> to_name;
 
-        if (std::string(name) == from_name) {
-          return libc_gethostbyname2(to_name.c_str(), af);
+        if (lambda(from_name, to_name)) {
+          break;
         }
       }
       break;
     }
   }
-  return libc_gethostbyname2(name, af);
+}
+std::string conv_resolve_host(std::string pathname) {
+  std::string new_pathname_str(pathname);
+  parse_hosts([&](std::string from_name, std::string to_name) {
+    if (pathname == from_name) {
+      new_pathname_str = to_name;
+      return true;
+    }
+    return false;
+  });
+  return new_pathname_str;
+}
+
+extern "C" {
+int inet_aton(const char *cp, struct in_addr *inp) {
+  using libc_inet_aton_pointer_type                 = decltype(&inet_aton);
+  static libc_inet_aton_pointer_type libc_inet_aton = nullptr;
+  if (libc_inet_aton == nullptr) {
+    libc_inet_aton = (libc_inet_aton_pointer_type)dlsym(RTLD_NEXT, "inet_aton");
+    assert(libc_inet_aton && "failed dlsym inet_aton");
+  }
+
+  return libc_inet_aton(conv_resolve_host(std::string(cp)).c_str(), inp);
+}
+
+struct hostent *gethostbyname2(const char *name, int af) {
+  using libc_gethostbyname2_pointer_type                      = decltype(&gethostbyname2);
+  static libc_gethostbyname2_pointer_type libc_gethostbyname2 = nullptr;
+  if (libc_gethostbyname2 == nullptr) {
+    libc_gethostbyname2 = (libc_gethostbyname2_pointer_type)dlsym(RTLD_NEXT, "gethostbyname2");
+    assert(libc_gethostbyname2 && "failed dlsym gethostbyname2");
+  }
+
+  return libc_gethostbyname2(conv_resolve_host(std::string(name)).c_str(), af);
 }
 }
